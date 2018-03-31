e843126768c89a1cdf54681c62dad4593459f474
Tony Borisov <tony.d.borisov@gmail.com>
2018-03-31 06:55:46 +0300

    feat: add function that shows a card on click

--- js/app.js
+++ js/app.js
@@ -36,3 +36,20 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+$(document).ready(function() {
+    function showCard(card) {
+        const show = "card open show";
+
+        $(card).attr('class', show);
+    }
+    
+    $('ul').on('mousedown', 'li', function(){
+        showCard(this);
+    }).on('mouseup', 'li', function(){
+        // TODO
+        // checkMatch(this);
+    });
+});
+
